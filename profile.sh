012d7e49ecb7e3e8224e2fad252512ce6bb3bbb0
Jakob Gillich <jakob@gillich.me>
2026-01-28 13:37:21 +0100

    chore: update profile.sh

--- profile.sh
+++ profile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./gradlew mainBenchmarkJar
+./gradlew mainBenchmarkJar -PbenchmarksInclude="beerouter"
 cd benchmark
 java -jar build/benchmarks/main/jars/benchmark-main-jmh-1.7.8-JMH.jar -prof stack
 #java -jar build/benchmarks/main/jars/benchmark-main-jmh-1.7.8-JMH.jar -prof "async:libPath=/var/home/jgillich/Downloads/async-profiler-4.3-linux-x64/lib/libasyncProfiler.so;output=flamegraph;dir=profile-results"
